c9090de7e6b04c4f94ce0c03060c5d96600e0f22
badview08 <jonesgabriel21@gmail.com>
2015-08-01 22:00:45 -0500

    added logo, background image

---
+++ dist/scripts/templates.js
@@ -0,0 +1,4 @@
+this["JST"] = this["JST"] || {};
+this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<div class=\"main-container\">\n  <img class=\"logo-img\" src=\"images/logo.png\" alt=\"\" />\n</div>\n";
+},"useData":true});
