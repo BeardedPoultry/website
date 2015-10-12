a59b81aef4bd681359c3bf495732163f6f3c19cf
badview08 <jonesgabriel21@gmail.com>
2015-10-12 11:47:51 -0400

    removed social links and redoing layout

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -6,6 +6,10 @@ require.register("main", function(exports, require, module){
 
   $(document).ready(function () {
     $('body').prepend(JST.application());
+    $('.show-menu').on('click', function (e) {
+      console.log("showmenu");
+      $('.menu-items').toggleClass('is-active');
+    });
   });
 })();
   
--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,11 @@
 
   $(document).ready(function(){
     $('body').prepend(JST.application());
+    $('.show-menu').on('click', function(e){
+    console.log("showmenu")
+  $('.menu-items').toggleClass('is-active');
+    });
   });
+  
+  
 })();
