1812c549dbaffa8f9bb65b792212830791031e0c
badview08 <jonesgabriel21@gmail.com>
2015-10-13 13:35:16 -0400

    added closing buttons

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -6,17 +6,24 @@ require.register("main", function(exports, require, module){
 
   $(document).ready(function () {
     $('body').prepend(JST.application());
+
     $('.show-portfolio').on('click', function (e) {
-      console.log("showmenu");
       $('.menu-portfolio').toggleClass('is-active');
     });
+
     $('.show-about').on('click', function (e) {
-      console.log("showmenu");
       $('.menu-about').toggleClass('is-active');
     });
+
     $('.show-contact').on('click', function (e) {
-      console.log("showmenu");
       $('.menu-contact').toggleClass('is-active');
+    });
+
+    $('.body').on('click', function (e) {
+
+      $('.menu-portfolio').toggleClass('is-active');
+      $('.menu-contact').toggleClass('is-active');
+      $('.menu-about').toggleClass('is-active');
     });
   });
 })();
